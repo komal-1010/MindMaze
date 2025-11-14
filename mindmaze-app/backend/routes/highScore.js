d966c977677f3aff59a216c5e9e394b69653bbbd
dhokkomal10 <dhokkomal10@gmail.com>
2025-11-14 15:41:37 +0000

    added dependency

--- mindmaze-app/backend/routes/highScore.js
+++ mindmaze-app/backend/routes/highScore.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import User from '../models/User.js';
 import authMiddleware from '../middleware/auth.js';
-import router from './autoRoutes.js';
+const router = express.Router();
 import Score from '../models/Score.js';
 
 router.get('/', authMiddleware, async (req, res) => {
